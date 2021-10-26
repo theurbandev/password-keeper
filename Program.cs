15e135f2b17f722221d23cd76d71db0ee6974e5e
Matthew Joseph <matthew.j.joe@gmail.com>
2021-10-26 00:14:15 -0400

    Major commit boiii
    -Got the input for user name
    -Get time of the day to give custom greeting

--- Program.cs
+++ Program.cs
@@ -6,7 +6,38 @@ namespace password_keeper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Greetings();
+        }
+
+        static void Greetings()
+        {
+            string userName;
+            string[] greetingsArr = { "Good Morning, ", "Good Afternoon, ", "Good Evening, " };
+
+
+            Console.WriteLine("What is your name...");
+            userName = Console.ReadLine();
+
+
+            // Serve custom greeting based on time of day.
+            string timeOfDay = getTimeOfDay();
+
+            // Assign greeting based on timeOfDay value returned.
+            
+
+            Console.WriteLine("\n" + greetingsArr[0] + userName + '.');
+            Console.WriteLine(timeOfDay);
+        }
+
+        static string getTimeOfDay()
+        {
+            string timeOfDay;
+            DateTime currentDateTime = new DateTime();
+
+            // extracts time (including am/pm from dateTime as a string 
+            timeOfDay = currentDateTime.ToShortTimeString(); 
+;
+            return timeOfDay; 
         }
     }
 }
