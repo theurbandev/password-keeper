d09bbe0a3dde4ec611e4e0f91d0c2968ca91bc1a
Matthew Joseph <matthew.j.joe@gmail.com>
2021-10-31 19:51:51 -0400

    Custom greeting based on time of day.

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ namespace password_keeper
         static void Greetings()
         {
             string userName;
-            string[] greetingsArr = { "Good Morning, ", "Good Afternoon, " };
+            bool password = false;
+            string[] greetingsArr = { "Good Morning, ", "Good Afternoon, ", "Good Evening, " };
 
 
             Console.WriteLine("What is your name...");
@@ -24,36 +25,44 @@ namespace password_keeper
 
             // Assign greeting based on timeOfDay value returned.
             string dayTypeCode = timeOfDay.Substring(timeOfDay.Length - 2);
-            if(dayTypeCode == "AM")
+            string firstDigitTime = timeOfDay.Substring(0, 1);
+
+            if (dayTypeCode == "AM")
             {
                 Console.WriteLine("\n" + greetingsArr[0] + userName + '.');
-            }else if(dayTypeCode == "PM")
+            }
+            else if (dayTypeCode == "PM" && int.Parse(firstDigitTime) < 5)
             {
                 Console.WriteLine("\n" + greetingsArr[1] + userName + '.');
             }
+            else if (dayTypeCode == "PM" && int.Parse(firstDigitTime) > 5)
+            {
+                Console.WriteLine("\n" + greetingsArr[2] + userName + '.');
+            }
 
             // Password setup
-            Console.WriteLine("Looks like this is your first time using this application." + "\n" + "It's time to setup your password: " + "\n");
-            string userPassword = Console.ReadLine();
-
-            // Account creation
-            accountCreation();
+            if (password){
+                
+            }else{
+                accountCreation();
+            }
         }
 
         static string getTimeOfDay()
         {
             string timeOfDay;
-            DateTime currentDateTime = new DateTime();
+            string currentDateTime = DateTime.Now.ToString("h:mm:ss tt");
 
             // extracts time (including am/pm from dateTime as a string 
-            timeOfDay = currentDateTime.ToShortTimeString(); 
+            timeOfDay = currentDateTime; 
 
             return timeOfDay; 
         }
 
         static void accountCreation()
         {
-
+            Console.WriteLine("Looks like this is your first time using this application." + "\n" + "It's time to setup your password: " + "\n");
+            string userPassword = Console.ReadLine();
         }
     }
 }
