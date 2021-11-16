2f07ac94641e2334b2beb7a3bcbb8054647f00e7
Matthew Joseph <matthew.j.joe@gmail.com>
2021-11-16 08:09:41 -0500

    Connection strings to sql

---
+++ db/constants.cs
@@ -0,0 +1,12 @@
+﻿// Connections strings used in connections.cs
+
+namespace DataConnections
+{
+    public static class Data
+    {
+        public static string server = "";
+        public static string dabaseName = "password_keeper";
+        public static string username = "admin";
+        public static string password = "password123";
+    }
+}
