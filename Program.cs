104519e5dabc89dd9354dce2a921379bd84df39d
Matthew Joseph <matthew.j.joe@gmail.com>
2021-12-01 13:58:44 -0500

    added logic for checking master username and password to db

--- Program.cs
+++ Program.cs
@@ -16,39 +16,71 @@ namespace password_keeper
         static void Greetings()
         {
             string userName;
-            bool password = false;
+            bool password = accountExist();
             string[] greetingsArr = { "Good Morning, ", "Good Afternoon, ", "Good Evening, " };
 
-
-            Console.WriteLine("What is your name...");
-            userName = Console.ReadLine();
-
-
-            // Gets string retuned from getTimeOfDay method.
-            string timeOfDay = getTimeOfDay();
-
-            // Assign greeting based on timeOfDay value returned.
-            string dayTypeCode = timeOfDay.Substring(timeOfDay.Length - 2);
-            string DigitTime = timeOfDay.Substring(0, 2);
-
-            if (dayTypeCode == "AM")
+            if (password)
             {
-                Console.WriteLine("\n" + greetingsArr[0] + userName + '.');
+                userActiveGreeting(password);
             }
-            /*else if (dayTypeCode == "PM" && int.Parse(DigitTime) < 5)
+            else
             {
-                Console.WriteLine("\n" + greetingsArr[1] + userName + '.');
-            } */
-            else if (dayTypeCode == "PM")
-            {
-                Console.WriteLine("\n" + greetingsArr[2] + userName + '.');
-            }
-                        
+                Console.WriteLine("----- Welcome to password manager. One applicaiton to manange all your passwords! -----" + "\n");
+                Console.WriteLine("What is your name: ");
+                userName = Console.ReadLine();
 
+                // Gets string retuned from getTimeOfDay method.
+                string timeOfDay = getTimeOfDay();
+
+                // Assign greeting based on timeOfDay value returned.
+                string dayTypeCode = timeOfDay.Substring(timeOfDay.Length - 2);
+                string DigitTime = timeOfDay.Substring(0, 2);
+
+                if (dayTypeCode == "AM")
+                {
+                    Console.WriteLine("\n" + greetingsArr[0] + userName + '.');
+                }
+                else if (dayTypeCode == "PM")
+                {
+                    Console.WriteLine("\n" + greetingsArr[2] + userName + '.');
+                }
+
+                userActiveGreeting(password);
+            }
+        }
+
+        static void userActiveGreeting(bool password)
+        {
             // Password setup
-            if (password){
-                //ask for password 
-            }else{
+            if (password)
+            {
+                //query to check if there is an account under users table
+                Console.WriteLine("Welcome back,");
+
+                Console.WriteLine("What is your username:");
+                string usernameSubmited = Console.ReadLine();
+
+                Console.WriteLine("\n" + "What is your password?");
+                string passwordSubmited = Console.ReadLine();
+
+                bool pwd_user_valid = validateCreds(passwordSubmited, usernameSubmited);
+
+                while (!pwd_user_valid)
+                {
+                    Console.WriteLine("__________");
+                    Console.WriteLine("Wrong credentials. Try again.." + "\n");
+
+                    Console.WriteLine("Username:");
+                    usernameSubmited = Console.ReadLine();
+
+                    Console.WriteLine("Password:");
+                    passwordSubmited = Console.ReadLine();
+                    pwd_user_valid = validateCreds(passwordSubmited, usernameSubmited);
+                }
+                Console.WriteLine("");
+            }
+            else
+            {
                 string pwd = passwordCreation();
                 string username = usernameCreation();
 
@@ -57,6 +89,70 @@ namespace password_keeper
 
                 accountDB_save(username, pwd);
             }
+        }
+
+        static bool validateCreds(string passwordSubmitted, string usernameSubmited)
+        {
+            var dbCon = DBConnection.Instance();
+            if (dbCon.IsConnect())
+            {
+                string query = $"SELECT user_name, user_password FROM USERS";
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                cmd.ExecuteNonQuery();
+
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    object db_username = rdr[0];
+                    object db_password = rdr[1];
+
+                    if (passwordSubmitted == db_password.ToString() && usernameSubmited == db_username.ToString())
+                    {
+                        rdr.Close();
+                        return true;
+                    }
+                    else
+                    {
+                        rdr.Close();
+                        return false;
+                    }
+                }
+            }
+            return false;
+        }
+
+        static bool accountExist()
+        {
+            //check if an account exisit in users table
+            var dbCon = DBConnection.Instance();
+            if (dbCon.IsConnect())
+            {
+                string query = $"SELECT id FROM USERS";
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                cmd.ExecuteNonQuery();
+
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    object data = rdr[0];
+                    if (data != null)
+                    {
+                        rdr.Close();
+                        return true;
+                    }
+                    else
+                    {
+                        rdr.Close();
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Connection failed...Data not saved");
+            }
+
+            return false;
         }
 
         static string getTimeOfDay()
@@ -80,7 +176,7 @@ namespace password_keeper
 
         static string usernameCreation()
         {
-            Console.WriteLine("\n" + "What would you like your username to be?");
+            Console.WriteLine("\n" + "What would you like your username to be: ");
             string userName = Console.ReadLine();
 
             return userName;
@@ -99,28 +195,17 @@ namespace password_keeper
                 var cmd = new MySqlCommand(query, dbCon.Connection);
                 cmd.ExecuteNonQuery();
 
-                //sqlTestMethod();
-                dbCon.Close();
-
+                //dbCon.Close();
                 Console.WriteLine("Data saved...");
+
+                //clear the screen
+                Console.WriteLine("Clearing the screen...");
+                Console.Clear();
+                Greetings();
             }
             else
             {
-                Console.WriteLine("Connection failed...");
-            }
-        }
-        
-        static void sqlTestMethod()
-        {
-            var dbCon = DBConnection.Instance();
-            string query2 = "SELECT * FROM USERS";
-            var cmd = new MySqlCommand(query2, dbCon.Connection);
-
-            var reader = cmd.ExecuteReader();
-
-            while (reader.Read())
-            {
-            Console.WriteLine("Users: " + reader.GetString(1));
+                Console.WriteLine("Connection failed...Data not saved");
             }
         }
     }
